422b0d5b13e90ca04ec7c6f60b9202ec52c2ca1c
Johnny <me@johnnythompson.co.uk>
2022-04-21 13:57:48 +0100

    added privacy policy endpoint, and can now use multiple keys and cycle through

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const axios = require('axios')
 require('dotenv').config()
 
-const app = express()
+const app = express();
+const path = require('path');
 
 app.use(cors())
 
@@ -12,7 +13,13 @@ app.get('/orchestrator', (req,res) => {
     res.json('welcome!')
 })
 
+const openWeatherMapKeys = process.env.REACT_APP_OPEN_WEATHER_API_KEYS.split(",");
+let openWeatherMapIterator = 0;
 app.get('/orchestrator/weather', (req,res) => {
+    if(openWeatherMapIterator >= openWeatherMapKeys.length)
+        openWeatherMapIterator = 0;
+    const key = openWeatherMapKeys[openWeatherMapIterator++];
+
     const lat = req.query.lat
     const lon = req.query.lon
 
@@ -23,14 +30,14 @@ app.get('/orchestrator/weather', (req,res) => {
           lat: lat,
           lon: lon,
           units: 'metric',
-          appid: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
+          appid: key,
         },
     }
 
     axios.request(options).then((response) => {
         res.json(response.data)
     }).catch((error) => {
-        console.error(error)
+        console.error(error.message+" using key "+key);
     })
 })
 
@@ -38,5 +45,8 @@ app.get('/orchestrator/api-key/google-places', (req,res) => {
     res.json(process.env.REACT_APP_GOOGLE_MAPS_PLACES_API_KEY)
 })
 
+app.get('/orchestrator/privacy/weather-we-go', (req,res) => {
+    res.sendFile(path.join(__dirname+'/weather-we-go-privacy.html'));
+})
 
 app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
